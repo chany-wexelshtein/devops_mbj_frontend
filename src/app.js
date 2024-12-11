d58117baa8e7f3d9255ddb56b002e9cc38a6161d
וקסלשטיין דבורה חנה2 <  2@mbjcomp.local>
2024-12-11 14:08:40 +0200

    Change made

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   const fetchData = () => {
-    fetch('http://localhost:5000/api')
+    fetch('http://34.120.129.160:80/api')
       .then(response => response.json())
       .then(data => setMessage(data.message))
       .catch(error => console.error('Error fetching data:', error));
@@ -15,7 +15,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Welcome to chany wexelshtein React App</h1>
-        <button /*onClick={fetchData}*/ className="fetch-button">Fetch backend</button>
+        <button onClick={fetchData} className="fetch-button">Fetch backend</button>
         <p>{message}</p>
       </header>
     </div>
